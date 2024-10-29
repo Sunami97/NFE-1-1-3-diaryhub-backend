e0e3fe8db3966b3853e02fb889afd64487611c01
Sunami97 <sunami7191@daum.net>
2024-10-29 11:19:09 +0900

    FIX 라이트 주소 변경

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors({
 app.use(express.json());
 
 // 정적 파일 경로 설정 (이미지 제공)
-app.use('uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB 연결
 mongoose.connect(MONGO_URI)
