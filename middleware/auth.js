d8cde162f7ddaa6c82ac9efd5cd3f6635f65ac28
Sunami97 <sunami7191@daum.net>
2024-11-06 15:13:13 +0900

    FEAT 인증 옵션 추가

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { JWT_SECRET } = process.env;
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) return res.status(401).json({ message: '토큰이 필요합니다.' });
 
@@ -16,5 +16,26 @@ module.exports = (req, res, next) => {
     } catch (error) {
         res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
     }
+};
+
+const authMiddlewareOptional = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
+            }
+            req.user = decoded; // 토큰에서 user 정보를 설정
+            next();
+        });
+    } else {
+        next(); // 토큰이 없는 경우에도 통과
+    }
+};
+
+module.exports = {
+    authMiddleware,
+    authMiddlewareOptional,
 };
 
